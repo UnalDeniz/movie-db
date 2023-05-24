fed34d62b83a934feae7d786c21efe0d6488eb31
Deniz <den1z.unal@protonmail.com>
2023-05-24 11:23:03 +0300

    added list_director api

--- backend/api/controllers/db_manager/index.js
+++ backend/api/controllers/db_manager/index.js
@@ -2,3 +2,4 @@ export { default as addDirector } from './add_director.js';
 export { default as addAudience } from './add_audience.js';
 export { default as deleteAudience } from './delete_audience.js';
 export { default as updateDirector } from './update_director.js';
+export { default as listDirector } from './list_director.js';
