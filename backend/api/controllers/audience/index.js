a182ddc2fdb84682a9c94f90335cfeeb0fbb50ea
Deniz <den1z.unal@protonmail.com>
2023-05-29 13:10:25 +0300

    added the rate functionality to audience

--- backend/api/controllers/audience/index.js
+++ backend/api/controllers/audience/index.js
@@ -1,3 +1,4 @@
 export { default as listMovie } from './list_movie.js';
 export { default as buyTicket } from './buy_ticket.js';
 export { default as listTicket } from './list_ticket.js';
+export { default as rateMovie } from './rate_movie.js';
